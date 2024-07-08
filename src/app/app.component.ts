5b89e7ab09532ff3fae9b56e7d2458dcd1a291d3
Hung Truong <hungtruongquoc@gmail.com>
2024-07-08 10:59:52 -0500

    Added a new page for showing asset list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
-import {Component, Inject, Injectable, makeStateKey, OnInit, Optional, PLATFORM_ID, TransferState} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  inject,
+  Inject,
+  Injectable,
+  makeStateKey,
+  OnInit,
+  Optional,
+  PLATFORM_ID,
+  TransferState
+} from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {SERVER_DATA} from "./tokens/server-data";
 import {HttpClient} from "@angular/common/http";
 import {isPlatformBrowser} from "@angular/common";
@@ -9,6 +19,8 @@ import {loadUser} from "./state/user.actions";
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import {NzIconDirective} from "ng-zorro-antd/icon";
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faHome} from "@fortawesome/pro-regular-svg-icons";
 
 const BEETRACK_USER = makeStateKey<string>('user-info');
 
@@ -16,17 +28,19 @@ const BEETRACK_USER = makeStateKey<string>('user-info');
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzLayoutModule, NzIconDirective, NzMenuModule],
+  imports: [RouterOutlet, NzLayoutModule, NzIconDirective, NzMenuModule, RouterLink, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit{
   isCollapsed: boolean = false;
+  faHome = faHome;
+  private router = inject(Router);
+
   constructor(@Optional() @Inject(SERVER_DATA) private serverData: any, private httpClient: HttpClient,
               private transferState: TransferState, private store: Store<{ user: UserState }>,
               @Inject(PLATFORM_ID) private platformId: Object) {
     if (serverData) {
-      console.log({serverData});
       this.httpClient.post("https://combuilding.beetrack.vn/api/v2/login", {username: this.serverData["beetrackUsername"], password: this.serverData["beetrackPassword"]})
         .subscribe((data: any) => {
           this.transferState.set(BEETRACK_USER, data["data"]);
