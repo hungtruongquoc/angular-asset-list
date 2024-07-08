7e798470f7c526bb035a73f3dddaa9398be07692
Hung Truong <hungtruongquoc@gmail.com>
2024-07-07 19:13:23 -0500

    Added fontawesome

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
+  faCoffee = faCoffee;
 }
